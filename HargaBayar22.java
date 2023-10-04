7db20b03d3a0be3ab674d1298ca25aae9e9b526d
JacktheRipper2187 <ulilf442@gmail.com>
2023-10-04 19:35:36 +0700

    revisi terbaru

--- HargaBayar22.java
+++ HargaBayar22.java
@@ -4,34 +4,34 @@ public class HargaBayar22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int harga, jumlah,jumlahHalaman;
-        double dis, total, bayar, jmlDis;
         String merkBuku;
-
+        int harga, jumlah,jmlHalBuku;
+        double diskon, total, bayar, jmlDiskon;
+        
         System.out.println("=====Selamat datang di toko wahyudi=====");
 
         System.out.println("Masukkan Merk Buku Anda = ");
         merkBuku = input.nextLine();
         System.out.println("Jumlah halaman buku = ");
-        jumlahHalaman = input.nextInt();
+        jmlHalBuku = input.nextInt();
         System.out.println("Masukkan berapa banyak buku yang dibeli = ");
         jumlah = input.nextInt();
         System.out.println("Masukkan harga buku anda = ");
-        harga = input.nextInt();
+        harga = input.nextInt();    
         System.out.println("Diskon yang anda dapatkan");
-        dis = input.nextInt();
+        diskon = input.nextDouble();
 
         total = harga * jumlah;
-        jmlDis = total * dis;
-        bayar = total - jmlDis;
+        jmlDiskon = total * diskon / 100;
+        bayar = total - jmlDiskon;
 
         System.out.println("=====Tampilkan Barang Yang Anda Beli=====");
 
         System.out.println("Merk buku = " + merkBuku);
-        System.out.println("Jumlah halaman buku = " + jumlahHalaman);
+        System.out.println("Jumlah halaman buku = " + jmlHalBuku);
         System.out.println("Jumlah yang anda beli = " + jumlah);
         System.out.println("Harga total sebelum diskon = " + total);
-        System.out.println("Jumlah diskon yang anda dapatkan adalah = " + jmlDis);
+        System.out.println("Jumlah diskon yang anda dapatkan adalah = " + jmlDiskon);
         System.out.println("Total yang dibayar setelah diskon = " + bayar + " rupiah ");
 
     
